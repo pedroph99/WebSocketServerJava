1ecebf479965289610afb3db6b81092f0f6ffa6a
pedroph99 <pedroph99@outlook.com>
2025-10-24 14:15:42 -0300

    Recebimento e envio de mensagens pelo servidor. Sistema de identificação de cada conexão

--- webSocketServer/src/webSocketServer/WebsocketServerInterface.java
+++ webSocketServer/src/webSocketServer/WebsocketServerInterface.java
@@ -2,6 +2,10 @@ package webSocketServer;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+
+// 
 public interface WebsocketServerInterface {
-	
+	void startServer();
+	void configureServer();
+	String[] getServerIPPort();
 }	
